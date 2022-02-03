15604087e2972d395f9b5fec88d73cdc9a6d2b7d
Vinicius <viniaraujo.1999@gmail.com>
2022-02-03 16:14:02 -0300

    StateHook com React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PaginaInicial from './componentes/Paginainicial';
+
+function App() {
+  return (
+    <div className="App">
+      <PaginaInicial />
+    </div>
+  );
+}
+
+export default App;
